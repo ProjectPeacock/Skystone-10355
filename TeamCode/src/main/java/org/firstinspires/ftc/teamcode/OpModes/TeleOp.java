f9d920e47ce253fa2c84371df9d9de1c5d001dbb
shbibly <alexcohenrichardson@gmail.com>
2019-10-31 20:04:43 -0500

    TeleOp program
    Updated: Mecanum driving program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
@@ -33,6 +33,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package org.firstinspires.ftc.teamcode.OpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 import org.firstinspires.ftc.teamcode.Hardware.HardwareProfile;
 
 /**
@@ -49,9 +50,9 @@ import org.firstinspires.ftc.teamcode.Hardware.HardwareProfile;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Test TeleOpMecanum", group = "Test")
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOp", group = "Comp")
 
-public class TeleOpMecanum extends LinearOpMode {
+public class TeleOp extends LinearOpMode {
     /**
      * Instantiate all objects needed in this class
      */
@@ -65,6 +66,8 @@ public class TeleOpMecanum extends LinearOpMode {
     private double strafeControl = 0;
     private double robotAngle =0;
     private double rightX = 0;
+    private double rightY = 0;
+    private double powerLevel = 0.5;
 
     @Override
     public void runOpMode() {
@@ -96,11 +99,12 @@ public class TeleOpMecanum extends LinearOpMode {
             strafeControl = gamepad1.left_stick_x;
             robotAngle = Math.atan2(gamepad1.left_stick_y, (gamepad1.left_stick_x * -1)) - Math.PI / 4;
             rightX = gamepad1.right_stick_x;
+            rightY = gamepad1.right_stick_y;
             r = Math.hypot((gamepad1.left_stick_x * -1), gamepad1.left_stick_y);
-            v1 = r * Math.cos(robotAngle) + rightX;
-            v2 = r * Math.sin(robotAngle) - rightX;
-            v3 = r * Math.sin(robotAngle) + rightX;
-            v4 = r * Math.cos(robotAngle) - rightX;
+            v1 = (r * Math.cos(robotAngle) + rightX + rightY) * powerLevel;
+            v2 = (r * Math.sin(robotAngle) - rightX + rightY) * powerLevel;
+            v3 = (r * Math.sin(robotAngle) + rightX + rightY) * powerLevel;
+            v4 = (r * Math.cos(robotAngle) - rightX + rightY) * powerLevel;
 
             /**
              * Erick updated code!
